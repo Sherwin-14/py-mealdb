1c420815421bb99fe43fe14a326aed85eb0bd01e
Sherwin-14 <varghesesheriwn@gmail.com>
2025-01-19 10:36:54 +0530

    extracted meal dictionary

--- mealdb/__init__.py
+++ mealdb/__init__.py
@@ -1,37 +1,40 @@
 import httpx
 
 
-
 def get_meal_by_name(name):
 
-    r = httpx.get(f'www.themealdb.com/api/json/v1/1/search.php?s={name}')
+    r = httpx.get(f'https://www.themealdb.com/api/json/v1/1/search.php?s={name}')
+    data = r.json()
+    meals = data['meals']
     
-    return r.json()
+    return list(meals)
 
 
 def list_all_meals(letter):
 
-    r = httpx.get(f'www.themealdb.com/api/json/v1/1/search.php?f={letter}')
+    r = httpx.get(f'https://www.themealdb.com/api/json/v1/1/search.php?f={letter}')
+    data = r.json()
+    meals = data['meals']
     
-    return r.json()
+    return list(meals)
 
 
 def meal_details_by_id(id):
 
-    r = httpx.get(f'www.themealdb.com/api/json/v1/1/lookup.php?i={id}')
+    r = httpx.get(f'https://www.themealdb.com/api/json/v1/1/lookup.php?i={id}')
     
     return r.json()
 
 
 def single_random_meal():
 
-    r = httpx.get(f'www.themealdb.com/api/json/v1/1/random.php')
+    r = httpx.get(f'https://www.themealdb.com/api/json/v1/1/random.php')
     
     return r.json()
 
 def list_meal_categories():
 
-    r = httpx.get(f'www.themealdb.com/api/json/v1/1/categories.php')
+    r = httpx.get(f'https://www.themealdb.com/api/json/v1/1/categories.php')
     
     return r.json()
 
---
+++ test.py
@@ -0,0 +1,7 @@
+import mealdb as mb
+
+meal = mb.get_meal_by_name("Arrabiata")
+list_meal = mb.list_all_meals("a")
+
+#print(meal)
+print(list_meal)
