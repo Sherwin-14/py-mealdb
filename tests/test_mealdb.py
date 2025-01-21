9ef512da5fc2e77872189170cc4ae730e88dc38d
Sherwin-14 <varghesesheriwn@gmail.com>
2025-01-21 12:01:47 +0530

    wrote 2nd set of functions

--- tests/test_mealdb.py
+++ tests/test_mealdb.py
@@ -13,3 +13,20 @@ class TestMealDB(unittest.TestCase):
         response = self.meal_db.get_meal_by_name(name)
         self.assertIsInstance(response, list)
         self.assertGreater(len(response), 0)
+
+     def test_meal_details_by_id(self):
+        id = 52772
+        response = self.meal_db.meal_details_by_id(id)
+        self.assertIsInstance(response, list)
+        self.assertGreater(len(response), 0)
+
+     def test_single_random_meal(self):
+        response = self.meal_db.single_random_meal()
+        self.assertIsInstance(response, list)
+        self.assertGreater(len(response), 0)
+
+     def test_list_all_meals(self):
+        letter = 'a'
+        response = self.meal_db.list_all_meals(letter)
+        self.assertIsInstance(response, list)
+        self.assertGreater(len(response), 0)
