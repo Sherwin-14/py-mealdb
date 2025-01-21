425c246fefc4d6b6480882135762b904e2f9d39c
Sherwin-14 <varghesesheriwn@gmail.com>
2025-01-21 12:00:53 +0530

    wrote class and first functions

--- tests/test_mealdb.py
+++ tests/test_mealdb.py
@@ -3,3 +3,13 @@ import httpx
 from mealdb import MealDB 
 
 class TestMealDB(unittest.TestCase):
+
+     def setUp(self):
+        self.api_key = 1
+        self.meal_db = MealDB(self.api_key)
+
+     def test_get_meal_by_name(self):
+        name = 'Arrabiata'
+        response = self.meal_db.get_meal_by_name(name)
+        self.assertIsInstance(response, list)
+        self.assertGreater(len(response), 0)
